30d88193461cbfc20c9dd07ab6c9589af42b718e
Alexsteele4121 <63519706+Alexsteele4121@users.noreply.github.com>
2022-03-22 13:29:44 -0700

    Update weekly_email.py

--- weekly_email.py
+++ weekly_email.py
@@ -1,3 +1,4 @@
+# To install all the requirements: pip install -r requirements.txt
 import gspread
 from google.oauth2.service_account import Credentials
 from termcolor import cprint
@@ -7,26 +8,30 @@ import smtplib
 from time import sleep
 # To install all the requirements: pip install -r requirements.txt
 
+
 # Google Doc Info
-tutor_tracking_spreadsheet = 'Tutor Tracking YOUR NAME'
-student_roster_worksheet_name = 'Student Roster'
+tutor_tracking_spreadsheet = '<NAME OF YOUR TUTOR TRACKING SPREADSHEET>'
+student_roster_worksheet = 'Student Roster'
 student_roster_cells_range = 'A3:G'
 
+# Emails You Would Like To Prevent From Receiving Emails
+blacklist = ['Example@gmail.com']
+
 # Google Doc Credential Info
-credentials_file = r'PATH\TO\GOOGLE\API\CREDS'
+api_credentials_file = r'<PATH\TO\YOUR\API\CREDS>'  # Youtube video to help setup credentials: https://youtu.be/cnPlKLEGR7E
 api_scopes = [
     'https://www.googleapis.com/auth/spreadsheets',
     'https://www.googleapis.com/auth/drive'
 ]
 
 # Gmail Login Info
-gmail_username = 'YOUR GMAIL'
-gmail_app_password = 'YOUR GMAIL API PASSWORD'
+gmail_username = '<YOUR EMAIL>@instructors.2u.com'
+gmail_app_password = '<YOUR APP KEY>'  # You can create an app key here: https://myaccount.google.com/security
 
 # Weekly Message
-full_name = 'YOUR FULL NAME'
-calendly_link = 'LINK TO YOUR CALENDLY'
-weekly_subject = 'Cyber Boot Camp - Tutorial available'
+full_name = '<YOUR NAME>'
+calendly_link = '<YOUR CALENDLY LINK>'
+weekly_subject = 'Cyber Boot Camp - Tutorial Available'
 weekly_message = f'''Hi Everyone!
 
 I hope you had a great week! I have attached a link below to schedule another tutoring session if you wish. If you are already scheduled, please ignore this email.
@@ -48,6 +53,7 @@ CC Central Support on all email by always using REPLY ALL.
 Sincerely,
 {full_name}'''
 
+# Nothing To Fill Out Past This Point.
 
 class Gmail:
 
@@ -122,6 +128,9 @@ class Roster:
                                     timezone_difference=data[5]))
         return students
 
+    def get_student_count(self) -> int:
+        return len(self.students)
+
     def get_all_students(self) -> list:
         return self.students
 
@@ -129,32 +138,47 @@ class Roster:
         return [student.email for student in self.students]
 
 
+def get_roster():
+    return Roster(spreadsheet=tutor_tracking_spreadsheet,
+                  worksheet=student_roster_worksheet,
+                  cells_range=student_roster_cells_range,
+                  creds_file=api_credentials_file,
+                  scopes=api_scopes)
+
+
 def main():
-    global database  # Declared Global, so it can be used in python interactive mode
     cprint('[-] Loading Roster...', 'yellow')
     try:
-        database = Roster(spreadsheet=tutor_tracking_spreadsheet,
-                          worksheet=student_roster_worksheet_name,
-                          cells_range=student_roster_cells_range,
-                          creds_file=credentials_file,
-                          scopes=api_scopes)
+        database = get_roster()
     except gspread.SpreadsheetNotFound:
-        cprint(f'[!] Could Not Load Your Roster! Please Ensure You Shared The Spreadsheet With Your Service Account.',
-               'red')
+        cprint(f'[!] Could Not Load Your Roster.\n'
+               f'[!] Please Ensure You Have Shared The Spreadsheet With Your Service Account.', 'red')
         exit(1)
+    except gspread.exceptions.WorksheetNotFound:
+        cprint(f'[!] Could Not Load Your Worksheet.\n'
+               f'[!] The Spreadsheet Was Accessible But No Worksheet Named "{student_roster_worksheet}" Was Found.',
+               'red')
+        exit(2)
+    except FileNotFoundError as e:
+        cprint(f'[!] Could Not Find Spreadsheet Credential File. {e}\n'
+               f'[!] Please Activate/Download Credentials: https://console.cloud.google.com', 'red')
+        exit(3)
     except Exception as e:
         cprint(f'[!] Fatal Error When Loading Roster. {e}', 'red')
-        exit(1)
-    cprint('[*] Roster Loaded Successfully!', 'green')
+        exit(4)
+    else:
+        cprint('[*] Roster Loaded Successfully!', 'green')
 
-    cprint('[-] Sending Weekly Emails...', 'yellow')
-    gmail = Gmail(gmail_username, gmail_app_password)
-    for student in database.get_all_students():
-        cprint(f'\r[-] Sending An Email To {student.name}'.ljust(50), 'yellow', end='')
-        gmail.email(Receiver=student.email, Message=weekly_message, Subject=weekly_subject,
-                    Cc='centraltutorsupport@bootcampspot.com')
-        sleep(.2)
-    cprint(f'\r[*] Emails Have Successfully Sent!'.ljust(50), 'green')
+        cprint('[-] Sending Weekly Emails...', 'yellow')
+        gmail = Gmail(gmail_username, gmail_app_password)
+        for student in database.get_all_students():
+            if student.email in blacklist:
+                continue
+            cprint(f'\r[-] Sending An Email To {student.name}'.ljust(50), 'yellow', end='')
+            gmail.email(Receiver=student.email, Message=weekly_message, Subject=weekly_subject,
+                        Cc='centraltutorsupport@bootcampspot.com')
+            sleep(2)
+        cprint(f'\r[*] Emails Have Successfully Sent!'.ljust(50), 'green')
 
 
 if __name__ == '__main__':
